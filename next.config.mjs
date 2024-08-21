23b6fa533906b9ba8eb079e509c40ef8065c0301
cmtlyt <cmt01964896@taobao.com>
2024-08-21 09:31:15 +0800

    feat: sass框架

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
   compiler: {
     styledComponents: true,
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src')],
   },
   reactStrictMode: false,
   experimental: {
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,11 @@
+import styles from './index.module.scss';
+
+export function Navbar() {
+  return <nav className={styles.navbarWrapper}>Navbar</nav>;
+}
+
+// 1220px main left-aside hidden
+// 1190px navbar tab shrink
+// 1000px main right-aside hidden
+// 800px navbar creater button hidden
+// 600px main not padding and width is 100vw
